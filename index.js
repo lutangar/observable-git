18e36d07d70cd3d580a866160cee9b79a33df909
lutangar <johan.dufour@gmail.com>
2018-06-19 12:33:19 +0200

    fix arguments handling

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { spawn } = require('observable-child-process');
 
-const createGit = cwd => args => spawn('git', [args], { cwd });
+const toArray = params => (Array.isArray(params) ? params : [params]);
+const createGit = cwd => (...args) => spawn('git', toArray(args), { cwd });
 
 module.exports = createGit;
